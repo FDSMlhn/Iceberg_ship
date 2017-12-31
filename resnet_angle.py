28c8f8cfafc1c9178dd5a1bc81e16087e65c9d89
“nannnth” <fdsm_lhn@yahoo.com>
2017-12-31 06:15:33 +0000

    minor change

--- resnet_angle.py
+++ resnet_angle.py
@@ -1,16 +1,6 @@
 import torch.nn as nn
+import torch
 import math
-
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152']
-
-def conv3x3(in_planes, out_planes, stride=1):
-    "3x3 convolution with padding"
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-
-
 import torch.utils.model_zoo as model_zoo
 
 
@@ -27,12 +17,16 @@ model_urls = {
 }
 
 
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None,dropout=0):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -41,8 +35,6 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
-        self.dropout=dropout
-        self.dp = nn.Dropout2d(p=self.dropout)
 
     def forward(self, x):
         residual = x
@@ -59,8 +51,6 @@ class BasicBlock(nn.Module):
 
         out += residual
         out = self.relu(out)
-        if self.dropout >0:
-            out = self.dp(out)
 
         return out
 
@@ -68,7 +58,7 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, dropout=0):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -80,10 +70,7 @@ class Bottleneck(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
-        self.dropout = dropout
-        self.dp = nn.Dropout2d(p=self.dropout)
 
-        
     def forward(self, x):
         residual = x
 
@@ -103,32 +90,38 @@ class Bottleneck(nn.Module):
 
         out += residual
         out = self.relu(out)
-        
-        if self.dropout >0:
-            out = self.dp(out)
 
         return out
 
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000,dropout=[0,0,0,0]):
+    def __init__(self, block, layers, num_classes=1000):
         self.inplanes = 64
-        self.dropout= dropout
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0],drop_out=self.dropout[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,drop_out=self.dropout[1])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,drop_out=self.dropout[2])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,drop_out=self.dropout[3])
-        self.avgpool = nn.AvgPool2d(5, stride=1)   # if input stride 1 output 5
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(5, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
-        #self.dp = nn.Dropout(p=0.5)
-        
+#         self.fc = nn.Sequential(
+#             nn.Linear(512 * block.expansion+1,256 * block.expansion),
+#             nn.BatchNorm1d(256 * block.expansion),
+#             nn.ReLU(True),
+#             nn.Dropout(0.4),
+#             nn.Linear(256 * block.expansion,256 * block.expansion),
+#             nn.BatchNorm1d(256 * block.expansion),
+#             nn.ReLU(True),
+#             nn.Dropout(0.6),
+#             nn.Linear(256 * block.expansion,num_classes)
+#         )
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -137,7 +130,7 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, drop_out,stride=1):
+    def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
@@ -147,14 +140,15 @@ class ResNet(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample,dropout=drop_out))
+        layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, dropout=drop_out))
+            layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        x, angle =x
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -164,10 +158,10 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        
+
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        #x = self.dp(x)
+        x = torch.cat((x,angle),1)
         x = self.fc(x)
 
         return x
